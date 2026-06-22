a9e57ceaa0db0bb9b9c4cf33553de42aec67cedf
JuampiBolanio <juampi360786@gmail.com>
2026-06-22 11:19:35 -0300

    feat(all):  inicializar proyecto con spring boot

---
+++ src/test/java/com/techfixsolutions/techfix/TechfixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techfixsolutions.techfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechfixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
