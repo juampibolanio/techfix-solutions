4496327ae356323a944174ab3afdfc35d8df5bef
JuampiBolanio <juampi360786@gmail.com>
2026-06-25 20:52:54 -0300

    refactor(user): eliminar la anotación @Repository en UserRepository

--- src/main/java/com/techfixsolutions/techfix/features/users/UserRepository.java
+++ src/main/java/com/techfixsolutions/techfix/features/users/UserRepository.java
@@ -3,9 +3,7 @@ package com.techfixsolutions.techfix.features.users;
 import com.techfixsolutions.techfix.features.users.models.User;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
 }
