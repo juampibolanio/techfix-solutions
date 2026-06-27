0e61b790d4bf59dc6f333078124b3d523b694a98
JuampiBolanio <juampi360786@gmail.com>
2026-06-27 20:04:07 -0300

    feat(category): agregar clase Category para gestión de categorías

---
+++ src/main/java/com/techfixsolutions/techfix/features/categories/models/Category.java
@@ -0,0 +1,37 @@
+package com.techfixsolutions.techfix.features.categories.models;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+
+    @Column(unique = true, nullable = false)
+    @NotBlank
+    private String name;
+
+    @Column(length = 150)
+    private String description;
+}
