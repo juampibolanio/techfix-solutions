f53e89449f50948273923b2bcb846c1138f3a59b
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-06-27 21:36:48 -0300

    feat(category): agregar metodos en servicio de categorias

--- src/main/java/com/techfixsolutions/techfix/features/categories/CategoryService.java
+++ src/main/java/com/techfixsolutions/techfix/features/categories/CategoryService.java
@@ -1,5 +1,29 @@
 package com.techfixsolutions.techfix.features.categories;
 
+import com.techfixsolutions.techfix.features.categories.dto.CategoryDto;
+import com.techfixsolutions.techfix.features.categories.models.Category;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
-    
+    private final CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> findOne(UUID uuid) {
+        return categoryRepository.findById(uuid);
+    }
+
+    public Category create(CategoryDto dto) {
+        Category newCategory = Category.builder().name(dto.name()).description(dto.description()).build();
+        return categoryRepository.save(newCategory);
+    }
 }
