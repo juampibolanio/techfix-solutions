dd5eb9983288f79d6a78b456a519472559a9b32f
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-07-13 13:34:35 -0300

    feat(category): agregar relacion con tickets en entidad categoria

--- src/main/java/com/techfixsolutions/techfix/features/categories/models/Category.java
+++ src/main/java/com/techfixsolutions/techfix/features/categories/models/Category.java
@@ -1,13 +1,11 @@
 package com.techfixsolutions.techfix.features.categories.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.techfixsolutions.techfix.features.tickets.models.Ticket;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -34,4 +32,7 @@ public class Category {
 
     @Column(length = 150)
     private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Ticket> tickets = new ArrayList<>();
 }
